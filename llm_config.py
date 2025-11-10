d70eed124c35bfa8fea50235143a731b04fc2a47
ohmycloud <ohmycloudy@gmail.com>
2025-11-10 17:24:13 +0800

    completions

--- llm_config.py
+++ llm_config.py
@@ -1,11 +1,13 @@
 from utils import openai_client
 
 response = openai_client.completions.create(
-    model="text-davinci-003",
+    model="gpt-3.5-turbo-instruct",
     prompt="I have a white dog named Champ.",
     temperature=1,
-    max_tokens=256,
+    max_tokens=16,
     top_p=0.3,
     frequency_penalty=0,
     presence_penalty=0
 )
+
+print(response.choices[0].text.strip())
